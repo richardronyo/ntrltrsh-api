7711a7e43fd3c3b14a80e6a5c511f022f0d52e4c
richardronyo <richardronyo@gmail.com>
2024-10-06 21:04:18 -0600

    Added login route. Will return TRUE if login information is correct, FALSE otherwise

--- api/lib/signup/signup_funcs.py
+++ api/lib/signup/signup_funcs.py
@@ -2,6 +2,7 @@ from api import models, db
 
 def add_login_information(user):
     """
+    This function adds a row to the LoginInformation table
     {
         "ACCOUNT_TYPE": <account_type>,
         "USERNAME": <username>,
@@ -20,3 +21,25 @@ def add_login_information(user):
     db.session.commit()
 
     return True
+
+def login_verification(user):
+    """
+    This function will check if the username and password sent in the JSON is the same as one in the database.
+    {
+        "USERNAME_OR_EMAIL": <username or email>,
+        "PASSWORD": <password>
+    }
+    """
+    username_or_email = user["USERNAME_OR_EMAIL"]
+    password = user["PASSWORD"]
+
+    #These lines will return the row that the username or email provided by the user is found. They will return None if otherwise
+    user_email = models.LoginInformation.query.filter(models.LoginInformation.email == username_or_email).one_or_none()
+    user_username = models.LoginInformation.query.filter(models.LoginInformation.username == username_or_email).one_or_none()
+
+    #If the username/email is correct, and it matches the stored password, the user can successfully login
+    if ((user_email is not None and user_email.password == password) or (user_username is not None and user_username.password == password)):
+        return True
+    
+    return False
+
--- api/routes/signup/routes.py
+++ api/routes/signup/routes.py
@@ -2,10 +2,16 @@ from api.routes.signup import signup_route
 
 from flask import request, jsonify
 
-from api.lib.signup.signup_funcs import add_login_information
+from api.lib.signup.signup_funcs import add_login_information, login_verification
 
 
 @signup_route.route('/logininformation', methods=["POST"])
 def new_user():
     user = request.get_json()
     return jsonify(add_login_information(user)), 200
+
+@signup_route.route('/login', methods=["POST"])
+def login():
+    user = request.get_json()
+
+    return jsonify(login_verification(user))
