fd692eaa2a63300f7b2474bc0fbe4144191990ee
richardronyo <richardronyo@gmail.com>
2024-10-06 21:58:41 -0600

    Checks if a username/email is already in the database for login signup route

--- api/lib/signup/signup_funcs.py
+++ api/lib/signup/signup_funcs.py
@@ -15,6 +15,10 @@ def add_login_information(user):
     elif user['ACCOUNT_TYPE'] == 2:
         account_type = models.AccountType.STUDENT
 
+    #If there is a user with that username or email, return False
+    if len(models.LoginInformation.query.filter(models.LoginInformation.username == user["USERNAME"]).all()) == 1 or len(models.LoginInformation.query.filter(models.LoginInformation.email == user["EMAIL"]).all()) == 1:
+        return False
+    
     user = models.LoginInformation(user['EMAIL'], user['USERNAME'], user['PASSWORD'], account_type)
 
     db.session.add(user)
