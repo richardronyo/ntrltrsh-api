75a5d1867e38ea56c885b7cd8e9b32897bd7d634
turtiaks <turtiaks@mymacewan.ca>
2024-10-26 16:24:28 -0600

    Added hashing of the password to user sign up and login

--- api/lib/login/login_funcs.py
+++ api/lib/login/login_funcs.py
@@ -3,6 +3,7 @@ from api import models, db
 import jwt
 import datetime
 from flask import current_app
+from api.lib.Security.AESPython import hash_password_with_salt, split_salt_and_password
 
 def login_verification(user):
     """
@@ -19,13 +20,29 @@ def login_verification(user):
     }
     """
     username_or_email = user["USERNAME_OR_EMAIL"]
-    password = user["PASSWORD"]
+    password = user["PASSWORD"] #this is the current plaintext password, so we need to hash it so we can compare with the one in the database
 
-    # Check if username/email exists and verify password
+    # Check if username/email exists
     user_email = models.LoginInformation.query.filter(models.LoginInformation.email == username_or_email).one_or_none()
     user_username = models.LoginInformation.query.filter(models.LoginInformation.username == username_or_email).one_or_none()
 
-    if (user_email is not None and user_email.password == password) or (user_username is not None and user_username.password == password):
+    #To compare passwords, first we will need to get the correct password field from the database
+    if(user_email is not None):
+        #Get password associated with email in database
+        database_password = user_email.password
+    elif(user_username is not None):
+        #Get password associated with username in database
+        database_password = user_username.password
+    else:
+        return {"LOGIN": False} #Username / Email does not exist in the database
+
+    #Now, lets break the salt and password hash apart
+    database_salt, database_password_hash = split_salt_and_password(database_password)
+    #Now, hash the the user input password using the database salt
+    user_password_hash = hash_password_with_salt(database_salt, password)
+    
+    #Verify password
+    if (user_email is not None and database_password_hash == user_password_hash) or (user_username is not None and database_password_hash == user_password_hash):
         # User authenticated successfully, generate JWT token
         payload = {
             "user_id": user_email.id if user_email else user_username.id  # Include user_id or any user info you need
@@ -38,4 +55,4 @@ def login_verification(user):
             "token": token
         }
 
-    return {"LOGIN": False}
+    return {"LOGIN": False} #Username or email exist, but password doesn't match
