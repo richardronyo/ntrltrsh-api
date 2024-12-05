cc6ddecc2b01609fb0376bca31c13e012ef65cb3
wongh77 <wongh77@mymacewan.ca>
2024-12-04 21:42:18 -0700

    Route to verify and return email
    Can be given a username or email

--- api/lib/email/email_funcs.py
+++ api/lib/email/email_funcs.py
@@ -26,3 +26,29 @@ def send_email_login_failure(email):
         </html> 
         """ 
         mail.send(msg)
+
+def find_email(email):
+    """
+    This function determines if the email exists in database
+    """
+    #this function will take in a string, could be username or email
+    #assume email first, do a query/filter on loginInformation and return
+    #the email if variable matches entry in database
+    emailAddress = models.LoginInformation.query.filter(
+        models.LoginInformation.email == email
+    ).one_or_none()
+
+    #if no matches, assume username is entered
+    #username query to find account, then return attached email
+    if emailAddress is None:
+        emailAddress = models.LoginInformation.query.filter(
+        models.LoginInformation.username == email
+    ).one_or_none()
+        
+    #if email exists, return email String
+    if emailAddress is not None:
+        #emailAddress is LoginInformation type
+        return emailAddress.email
+    #if both does not exist, return false or similar (String type)
+    else:
+        return "False"
